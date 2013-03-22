afe66d9a2eb1cad47863b4c1f2545b5f2a193171
Owen Rodley <Strigoides@gmail.com>
2013-03-22 23:56:34 +1300

    Actually, spaces rather than tabs

--- RexSimulatorCLI/BasicSerialPort.cs
+++ RexSimulatorCLI/BasicSerialPort.cs
@@ -6,26 +6,26 @@ using RexSimulator.Hardware.Rex;
 
 namespace RexSimulatorCLI
 {
-	public class BasicSerialPort
-	{
-		private SerialIO mSerialPort;
+    public class BasicSerialPort
+    {
+        private SerialIO mSerialPort;
 
-		public BasicSerialPort (SerialIO port)
-		{
-			this.mSerialPort = port;
+        public BasicSerialPort (SerialIO port)
+        {
+            this.mSerialPort = port;
 
-			mSerialPort.SerialDataTransmitted +=
+            mSerialPort.SerialDataTransmitted +=
                 new EventHandler<SerialIO.SerialEventArgs>(mSerialPort_SerialDataTransmitted);
-		}
+        }
 
         /// <summary>
         /// Write data received from the serial port to stdout
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-		void mSerialPort_SerialDataTransmitted(object sender, SerialIO.SerialEventArgs e)
-		{
+        void mSerialPort_SerialDataTransmitted(object sender, SerialIO.SerialEventArgs e)
+        {
             Console.Write((char)e.Data);
-		}
-	}
+        }
+    }
 }
--- RexSimulatorCLI/RexSimulator.cs
+++ RexSimulatorCLI/RexSimulator.cs
@@ -7,106 +7,104 @@ using RexSimulator.Hardware;
 
 namespace RexSimulatorCLI
 {
-	public class RexSimulator
-	{
-		private const long TARGET_CLOCK_RATE = 4000000;
+    public class RexSimulator
+    {
+        private const long TARGET_CLOCK_RATE = 4000000;
 
-		private RexBoard mRexBoard;
-		private Thread mWorker;
+        private RexBoard mRexBoard;
+        private Thread mWorker;
 
-		private BasicSerialPort mSerialPort1;
-		//private BasicSerialPort mSerialPort2;
-				
-		private long mLastTickCount = 0;
-		private DateTime mLastTickCountUpdate = DateTime.Now;
-		public double mLastClockRate = TARGET_CLOCK_RATE;
-		private double mLastClockRateSmoothed = TARGET_CLOCK_RATE;
-		private bool mThrottleCpu = true;
-		
-		private bool mRunning = true;
-		private bool mStepping = false;
+        private BasicSerialPort mSerialPort1;
+        //private BasicSerialPort mSerialPort2;
+                
+        private long mLastTickCount = 0;
+        private DateTime mLastTickCountUpdate = DateTime.Now;
+        public double mLastClockRate = TARGET_CLOCK_RATE;
+        private double mLastClockRateSmoothed = TARGET_CLOCK_RATE;
+        private bool mThrottleCpu = true;
+        
+        private bool mRunning = true;
+        private bool mStepping = false;
 
-		public RexSimulator ()
-		{
-			mRexBoard = new RexBoard();
+        public RexSimulator ()
+        {
+            mRexBoard = new RexBoard();
 
-			//Load WRAMPmon into ROM
-			Stream wmon = new MemoryStream(ASCIIEncoding.ASCII.GetBytes(File.ReadAllText(Path.Combine("Resources", "monitor.srec"))));
-			mRexBoard.LoadSrec(wmon);
-			wmon.Close();
-			
-			//Set up the worker thread
-			mWorker = new Thread(new ThreadStart(Worker));
+            //Load WRAMPmon into ROM
+            Stream wmon = new MemoryStream(ASCIIEncoding.ASCII.GetBytes(File.ReadAllText(Path.Combine("Resources", "monitor.srec"))));
+            mRexBoard.LoadSrec(wmon);
+            wmon.Close();
+            
+            //Set up the worker thread
+            mWorker = new Thread(new ThreadStart(Worker));
 
-			// Set up the timer
-			// Qualified name is used since System.Threading also contains a class called "Timer"
-			System.Timers.Timer timer = new System.Timers.Timer();
-			timer.Elapsed += timer_Elapsed;
+            // Set up the timer
+            // Qualified name is used since System.Threading also contains a class called "Timer"
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Elapsed += timer_Elapsed;
 
-			timer.Enabled = true;
+            timer.Enabled = true;
 
+            //Set up system interfaces
+            mSerialPort1 = new BasicSerialPort(mRexBoard.Serial1);
 
-			//Set up system interfaces
-			mSerialPort1 = new BasicSerialPort(mRexBoard.Serial1);
+            mWorker.Start();
+        }
 
-			mWorker.Start();
-		}
+        private void Worker()
+        {
+            int stepCount = 0;
+            int stepsPerSleep = 0;
+            
+            while (true)
+            {
+                if (mRunning)
+                {
+                    this.Step();
+                    mRunning ^= mStepping; //stop the CPU running if this is only supposed to do a single step.
+                    
+                    //Slow the processor down if need be
+                    if (mThrottleCpu)
+                    {
+                        if (stepCount++ >= stepsPerSleep)
+                        {
+                            stepCount -= stepsPerSleep;
+                            Thread.Sleep(5);
+                            int diff = (int)mLastClockRate - (int)TARGET_CLOCK_RATE;
+                            stepsPerSleep -= diff / 10000;
+                            stepsPerSleep = Math.Min(Math.Max(0, stepsPerSleep), 1000000);
+                        }
+                    }
 
-		private void Worker()
-		{
-			int stepCount = 0;
-			int stepsPerSleep = 0;
-			
-			while (true)
-			{
-				if (mRunning)
-				{
-					this.Step();
-					mRunning ^= mStepping; //stop the CPU running if this is only supposed to do a single step.
-					
-					//Slow the processor down if need be
-					if (mThrottleCpu)
-					{
-						if (stepCount++ >= stepsPerSleep)
-						{
-							stepCount -= stepsPerSleep;
-							Thread.Sleep(5);
-							int diff = (int)mLastClockRate - (int)TARGET_CLOCK_RATE;
-							stepsPerSleep -= diff / 10000;
-							stepsPerSleep = Math.Min(Math.Max(0, stepsPerSleep), 1000000);
-						}
-					}
+                    if (Console.KeyAvailable)
+                    {
+                        char key = Console.ReadKey(true).KeyChar;
+                        mRexBoard.Serial1.SendAsync(key);
+                    }
+                }
+            }
+        }
 
-					if (Console.KeyAvailable)
-					{
-						char key = Console.ReadKey(true).KeyChar;
-						mRexBoard.Serial1.SendAsync(key);
-					}
-				}
+        /// <summary>
+        /// Recalculate the simulated CPU clock rate.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer_Elapsed(object sender, EventArgs e)
+        {
+            long ticksSinceLastUpdate = mRexBoard.TickCounter - mLastTickCount;
+            TimeSpan timeSinceLastUpdate = DateTime.Now.Subtract(mLastTickCountUpdate);
+            mLastTickCount = mRexBoard.TickCounter;
+            mLastTickCountUpdate = DateTime.Now;
 
-			}
-		}
+            double rate = 0.5;
+            mLastClockRate = ticksSinceLastUpdate / timeSinceLastUpdate.TotalSeconds;
+            mLastClockRateSmoothed = mLastClockRateSmoothed * (1.0 - rate) + mLastClockRate * rate;
+        }
 
-		/// <summary>
-		/// Recalculate the simulated CPU clock rate.
-		/// </summary>
-		/// <param name="sender"></param>
-		/// <param name="e"></param>
-		private void timer_Elapsed(object sender, EventArgs e)
-		{
-			long ticksSinceLastUpdate = mRexBoard.TickCounter - mLastTickCount;
-			TimeSpan timeSinceLastUpdate = DateTime.Now.Subtract(mLastTickCountUpdate);
-			mLastTickCount = mRexBoard.TickCounter;
-			mLastTickCountUpdate = DateTime.Now;
-
-			double rate = 0.5;
-			mLastClockRate = ticksSinceLastUpdate / timeSinceLastUpdate.TotalSeconds;
-			mLastClockRateSmoothed = mLastClockRateSmoothed * (1.0 - rate) + mLastClockRate * rate;
-		}
-
-		public void Step()
-		{
-			while (!mRexBoard.Tick()) ;
-		}
-	}
+        public void Step()
+        {
+            while (!mRexBoard.Tick()) ;
+        }
+    }
 }
