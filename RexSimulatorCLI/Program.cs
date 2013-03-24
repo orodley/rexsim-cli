d4a2a36f203289abdf950928acde7046cf76d578
Owen Rodley <Strigoides@gmail.com>
2013-03-25 11:49:32 +1300

    Check for an already running instance on startup

--- RexSimulatorCLI/Program.cs
+++ RexSimulatorCLI/Program.cs
@@ -1,17 +1,26 @@
 using System;
+using System.Diagnostics;
 
 namespace RexSimulatorCLI
 {
 	class Program
 	{
-        private static RexSimulator _rexSim;
-
 		public static void Main (string[] args)
 		{
-            //Set up console
-            Console.SetBufferSize(Console.WindowWidth, Console.WindowHeight);
+            RexSimulator rexSim;
+            
+            // Check if we're the only instance currently running
+		    Process otherInstance = null;
+            var current = Process.GetCurrentProcess();
 
-			_rexSim = new RexSimulator();
+            foreach (var process in Process.GetProcessesByName(current.ProcessName))
+                if (process.Id != current.Id)
+                    otherInstance = process;
+
+		    if (otherInstance != null && args.Length == 0)
+		        Console.WriteLine(@"Another instance is already running with PID: " + otherInstance.Id);
+            else
+                rexSim = new RexSimulator();
 		}
 	}
 }
