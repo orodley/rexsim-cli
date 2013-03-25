5482dd3e86df5000270467a1c1c153fc2c9028b9
Steven French <mr.steven.french@gmail.com>
2013-03-25 13:44:07 +1300

    Move getOtherProcess to static method

--- RexSimulatorCLI/Program.cs
+++ RexSimulatorCLI/Program.cs
@@ -3,24 +3,31 @@ using System.Diagnostics;
 
 namespace RexSimulatorCLI
 {
-	class Program
-	{
-		public static void Main (string[] args)
-		{
+    class Program
+    {
+        public static void Main (string[] args)
+        {
             RexSimulator rexSim;
-            
+
             // Check if we're the only instance currently running
-		    Process otherInstance = null;
+            Process otherInstance = getOtherInstance();
+
+            if (otherInstance != null && args.Length == 0)
+                Console.WriteLine(@"Another instance is already running with PID: " + otherInstance.Id);
+            else //New process; start the simulator
+                rexSim = new RexSimulator();
+	}
+
+        public static Process getOtherInstance()
+        {
+            Process otherInstance = null;
             var current = Process.GetCurrentProcess();
 
-            foreach (var process in Process.GetProcessesByName(current.ProcessName))
+            foreach (var process in Process.GetProcessesByName(curent.ProcessName))
                 if (process.Id != current.Id)
                     otherInstance = process;
 
-		    if (otherInstance != null && args.Length == 0)
-		        Console.WriteLine(@"Another instance is already running with PID: " + otherInstance.Id);
-            else
-                rexSim = new RexSimulator();
-		}
-	}
+            return otherInstance;
+        }
+    }
 }
